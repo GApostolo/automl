ab8c16336ff2518416c511acdf788adcb293ac58
Guilherme <g.apostolo@vu.nl>
2023-09-05 12:04:28 +0200

    Implementation for finetuned version, Panda Image dataset conversions, Mape calculation and inference modes scripts. Network Profiler not working, Remix need checking

--- efficientdet/model_generate.py
+++ efficientdet/model_generate.py
@@ -28,11 +28,16 @@ from ..efficientdet.model_inspect import ModelInspector
 def load_model_from_frozen_file(model_name: str = 'efficientdet-d0',
                                 saved_model_dir: str = './model/automl/efficientdet/model_save',
                                 min_score_thresh: int = 0.5, max_boxes_to_draw: int = 500, batch_size: int = 1,
-                                nms_method: str = 'hard'):
-    model_dir = saved_model_dir + "/" + model_name + "/" + model_name + "_frozen.pb"
+                                nms_method: str = 'hard', hparams_file_name: str = '/panda_config.yaml',
+                                finetune: bool = True):
+    if finetune:
+        model_dir = saved_model_dir + "/model_save/" + model_name + '-finetune' + "/" + model_name + "_frozen.pb"
+    else:
+        model_dir = saved_model_dir + "/" + model_name + "/" + model_name + "_frozen.pb"
+    hparams = saved_model_dir + hparams_file_name
     model_config = ModelInspector(model_name=model_name, saved_model_dir=model_dir, batch_size=batch_size,
                                   score_thresh=min_score_thresh, max_output_size=max_boxes_to_draw,
-                                  nms_method=nms_method)
+                                  nms_method=nms_method, hparams=hparams)
 
     driver = inference.ServingDriver(model_name, model_config.batch_size,
                                      model_params=model_config.model_config.as_dict(),
@@ -47,12 +52,23 @@ def load_model_from_frozen_file(model_name: str = 'efficientdet-d0',
 def load_model_from_checkpoint_file(model_name: str = 'efficientdet-d0',
                                     saved_model_dir: str = './model/automl/efficientdet/',
                                     min_score_thresh: int = 0.5, max_boxes_to_draw: int = 500, batch_size: int = 1,
-                                    nms_method: str = 'hard',
-                                    hparams: str = './model/automl/efficientdet/panda_config.yaml'):
-    model_dir = saved_model_dir + "/" + model_name + '-finetune'
+                                    nms_method: str = 'hard', hparams_file_name: str = '/panda_config.yaml',
+                                    finetune: bool = True):
+    if finetune:
+        model_dir = saved_model_dir + "/" + model_name + '-finetune'
+    else:
+        model_dir = saved_model_dir + "/" + model_name
+    hparams = saved_model_dir + hparams_file_name
     model_config = ModelInspector(model_name=model_name, saved_model_dir=model_dir, batch_size=batch_size,
                                   score_thresh=min_score_thresh, max_output_size=max_boxes_to_draw, hparams=hparams,
                                   nms_method=nms_method)
 
-    driver = inference.InferenceDriver(model_name, model_dir, model_params=model_config.model_config.as_dict())
+    driver = inference.ServingDriver(model_name, model_config.batch_size,
+                                     model_params=model_config.model_config.as_dict(),
+                                     min_score_thresh=min_score_thresh, max_boxes_to_draw=max_boxes_to_draw)
+    drive_start_time = time.perf_counter()
+    driver.ckpt_path = model_dir
+    driver.build()
+    drive_end_time = time.perf_counter()
+    print(f"Drive time (seconds): {drive_end_time - drive_start_time:.2f}")
     return driver
